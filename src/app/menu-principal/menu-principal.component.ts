e3275496b67843c9ffa8c648d63a07c01fad5d7d
yann97one <yannissileber@gmail.com>
2022-12-27 12:27:25 -0400

    finition de la page d'ajout de projets

--- src/app/menu-principal/menu-principal.component.ts
+++ src/app/menu-principal/menu-principal.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class MenuPrincipalComponent implements OnInit {
 
   constructor() { }
+  public date = new Date();
   public tab_menu = [
     "Projets",
     "Test"
--- src/app/page-projet/page-projet.component.ts
+++ src/app/page-projet/page-projet.component.ts
@@ -6,9 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['../app.component.scss','./page-projet.component.scss'],
 })
 export class PageProjetComponent implements OnInit {
-
+  date! : string;
+  comment! : string;
+  select! : string;
+  heure! : number;
+  IsDisabled! : boolean;
+  
+  
   constructor() { }
 
   ngOnInit() {}
-
+ 
+submit(){
+  
+}
 }
